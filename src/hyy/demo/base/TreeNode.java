1bfc26d4270a98b24f2a0bab72997b86df8ac4de
huangyanyan <huangyanyan@hmtforever.com>
2019-11-19 17:25:46 +0800

    二叉树层级遍历

---
+++ src/hyy/demo/base/TreeNode.java
@@ -0,0 +1,17 @@
+package hyy.demo.base;
+
+/**
+ * @author hyy
+ * @version V2.0, 2019/11/19
+ * @copyright
+ */
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
--- src/hyy/demo/levelTraversalOfBinaryTree.java
+++ src/hyy/demo/levelTraversalOfBinaryTree.java
@@ -1,7 +1,25 @@
 package hyy.demo;
 
+import hyy.demo.base.TreeNode;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 /**
- * TODO 〈一句话功能简述〉
+ * 给定一个二叉树，返回其按层次遍历的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 例如: 给定二叉树: [3,9,20,null,null,15,7],
+ *
+ *  3
+ * / \
+ * 9  20
+ *   /  \
+ *  15   7
+ *
+ * 返回其层次遍历结果：
+ *
+ * [ [3], [9,20], [15,7] ]
  *
  * @author hyy
  * @version V2.0, 2019/11/19
@@ -9,4 +27,35 @@ package hyy.demo;
  */
 public class levelTraversalOfBinaryTree {
 
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (null == root) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int size  = queue.size();
+
+        while (!queue.isEmpty()) {
+            List<Integer> list = new ArrayList<>();
+            while (size > 0){
+                TreeNode tmp = queue.poll();
+                list.add(tmp.val);
+                if (tmp.left != null){
+                    queue.add(tmp.left);
+                }
+                if (tmp.right != null){
+                    queue.add(tmp.right);
+                }
+                size--;
+            }
+            res.add(list);
+            if (!queue.isEmpty()){
+                size = queue.size();
+            }
+        }
+        return res;
+    }
+
 }
+
