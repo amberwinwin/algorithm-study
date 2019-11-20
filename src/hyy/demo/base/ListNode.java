d42339d6d71641f8bd99dc247d7db09720afa014
huangyanyan <huangyanyan@hmtforever.com>
2019-11-20 17:23:01 +0800

    反转链表

---
+++ src/hyy/demo/base/ListNode.java
@@ -0,0 +1,17 @@
+package hyy.demo.base;
+
+/**
+ * @author hyy
+ * @version V2.0, 2019/11/20
+ * @copyright
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+
+}
---
+++ src/hyy/demo/reverseList.java
@@ -0,0 +1,34 @@
+package hyy.demo;
+
+import hyy.demo.base.ListNode;
+
+/**
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL 输出: 5->4->3->2->1->NULL
+ *
+ * @author hyy
+ * @version V2.0, 2019/11/20
+ * @copyright
+ */
+public class reverseList {
+
+    public ListNode reverseList(ListNode head) {
+        if (null == head) {
+            return head;
+        }
+        ListNode pre = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = pre;
+            if (null == next) {
+                break;
+            }
+            pre = head;
+            head = next;
+        }
+        return head;
+    }
+}
