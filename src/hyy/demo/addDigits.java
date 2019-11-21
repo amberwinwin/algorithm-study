8dba6bf3da5c63b4d19a61ae86490f1278a58829
huangyanyan <huangyanyan@hmtforever.com>
2019-11-21 13:44:51 +0800

    各数位相加至个数

---
+++ src/hyy/demo/addDigits.java
@@ -0,0 +1,43 @@
+package hyy.demo;
+
+/**
+ * 给定一个非负整数 num，反复将各个位上的数字相加，直到结果为一位数。
+ *
+ * 示例:
+ *
+ * 输入: 38 输出: 2 解释: 各位相加的过程为：3 + 8 = 11, 1 + 1 = 2。 由于 2 是一位数，所以返回 2。
+ *
+ * @author hyy
+ * @version V2.0, 2019/11/20
+ * @copyright
+ */
+public class addDigits {
+
+    /*    public int addDigits(int num) {
+            int sum = 0;
+            while (num > 0){
+                sum += num%10;
+                num /= 10;
+                if (num  == 0){
+                    if (sum < 10){
+                        return sum;
+                    }
+                    num = sum;
+                    sum = 0;
+                }
+            }
+            return sum;
+        }*/
+
+    /**
+     * 把整数拆解成多项式去理解，剩余不能被9取模的数就是我们要的数
+     * @param num
+     * @return
+     */
+    public int addDigits(int num) {
+        if (num < 10){
+            return num;
+        }
+        return num % 9 == 0 ? 9 : num % 9;
+    }
+}
---
+++ src/hyy/demo/powerOfTwo.java
@@ -0,0 +1,33 @@
+package hyy.demo;
+
+/**
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 1 输出: true 解释: 20 = 1
+ *
+ * @author hyy
+ * @version V2.0, 2019/11/20
+ * @copyright
+ */
+public class powerOfTwo {
+    /* 另外的思路是按位与 */
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        if (n == 1) {
+            return true;
+        }
+        while (n > 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
