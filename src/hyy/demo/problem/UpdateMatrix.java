1ab426c7e2acb78e79c2682e5fd215d7e568dc06
huangyanyan <huangyanyan@hmtforever.com>
2020-01-22 11:57:03 +0800

    temporsry

---
+++ src/hyy/demo/problem/UpdateMatrix.java
@@ -0,0 +1,45 @@
+package hyy.demo.problem;
+
+/**
+ * 给定一个由 0 和 1 组成的矩阵，找出每个元素到最近的 0 的距离。
+ *
+ * 两个相邻元素间的距离为 1 。
+ *
+ * 示例 1: 输入:
+ *
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 输出:
+ *
+ * 0 0 0
+ * 0 1 0
+ * 0 0 0
+ * 示例 2: 输入:
+ *
+ * 0 0 0
+ * 0 1 0
+ * 1 1 1
+ * 输出:
+ *
+ * 0 0 0
+ * 0 1 0
+ * 1 2 1
+ * 注意:
+ *
+ * 给定矩阵的元素个数不超过 10000。 给定矩阵中至少有一个元素是 0。
+ * 矩阵中的元素只在四个方向上相邻: 上、下、左、右。
+ *
+ * @author hyy
+ * @version V2.0, 2020/1/22
+ * @copyright
+ */
+public class UpdateMatrix {
+
+    public int[][] updateMatrix(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int[][] distance = new int[row][col];
+        return distance;
+    }
+}
